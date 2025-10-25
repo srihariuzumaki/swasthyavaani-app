3c240869ad8727d3fc7edb51c1de29273d56689c
srihari <srihariamit@gmail.com>
2025-10-25 22:51:50 +0530

    fixed capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.89c74b0afc584b3995ad825a1f8fbe45',
-  appName: 'mediflow-design-system',
+  appId: 'com.swasthyavaani.app',
+  appName: 'Swasthya Vaani',
   webDir: 'dist',
   server: {
-    url: 'https://89c74b0a-fc58-4b39-95ad-825a1f8fbe45.lovableproject.com?forceHideBadge=true',
+    androidScheme: 'https',
     cleartext: true
   }
 };
