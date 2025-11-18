6dcc46816b2b061f471ea4ed799cff053dfbd58c
srihari <srihariamit@gmail.com>
2025-11-18 12:34:14 +0530

    fixed audio issue

--- android/app/src/main/java/com/swasthyavaani/app/MainActivity.java
+++ android/app/src/main/java/com/swasthyavaani/app/MainActivity.java
@@ -1,5 +1,29 @@
 package com.swasthyavaani.app;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            getBridge().getWebView().setWebChromeClient(new WebChromeClient() {
+                @Override
+                public void onPermissionRequest(final PermissionRequest request) {
+                    runOnUiThread(() -> {
+                        try {
+                            request.grant(request.getResources());
+                        } catch (Exception e) {
+                            request.deny();
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
