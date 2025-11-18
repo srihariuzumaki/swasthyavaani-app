295b01f8e6e7f6bbfefe8f3b29fd304edfffff95
srihari <srihariamit@gmail.com>
2025-11-18 13:18:19 +0530

    fixed mic issues

--- android/app/src/main/java/com/swasthyavaani/app/MainActivity.java
+++ android/app/src/main/java/com/swasthyavaani/app/MainActivity.java
@@ -2,28 +2,38 @@ package com.swasthyavaani.app;
 
 import android.os.Bundle;
 import android.webkit.PermissionRequest;
-import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebChromeClient;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getBridge() != null && getBridge().getWebView() != null) {
-            getBridge().getWebView().setWebChromeClient(new WebChromeClient() {
-                @Override
-                public void onPermissionRequest(final PermissionRequest request) {
-                    runOnUiThread(() -> {
-                        try {
-                            request.grant(request.getResources());
-                        } catch (Exception e) {
-                            request.deny();
-                        }
-                    });
-                }
-            });
+        if (getBridge() != null) {
+            WebView webView = getBridge().getWebView();
+            if (webView != null) {
+                webView.setWebChromeClient(new BridgeWebChromeClient(getBridge()) {
+                    @Override
+                    public void onPermissionRequest(final PermissionRequest request) {
+                        runOnUiThread(() -> {
+                            try {
+                                for (String resource : request.getResources()) {
+                                    if (PermissionRequest.RESOURCE_AUDIO_CAPTURE.equals(resource)) {
+                                        request.grant(new String[]{PermissionRequest.RESOURCE_AUDIO_CAPTURE});
+                                        return;
+                                    }
+                                }
+                                request.deny();
+                            } catch (Exception e) {
+                                request.deny();
+                            }
+                        });
+                    }
+                });
+            }
         }
     }
 }
